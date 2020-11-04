4e85f6e07221e70d121b02b218872ac90b1aaf01
hmida <mohamed.drissi123@yahoo.com>
2020-11-04 10:08:19 +0100

    f

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,10 +3,34 @@
 
 using namespace std;
 
-
 class Distance {
 public:
+	string sC;
 	float Xa, Ya, Xb, Yb, result;
+	Distance(float a1, float b1, float a2, float b2, string s) {
+		this->Xa = a1;
+		this->Ya = b1;
+		this->Xb = a2;
+		this->Yb = b2;
+		this->sC = s;
+	}
+	void check() {
+		if (sC == "all") {
+			all();
+		}
+		else if (sC == "Euc") {
+			euclidienne();
+		}
+		else if (sC == "Tcheb") {
+			Tchebychev();
+		}
+		else if (sC == "Man") {
+			Manhattan();
+		}
+		else {
+			cout << "Type de distance non reconnu.\n";
+		}
+	}
 	void euclidienne() {
 		result = sqrt(pow(Xb - Xa, 2) + pow(Yb - Ya, 2));
 		cout << "La distance euclidienne de (" << Xa << "," << Ya << ") et (" << Xb << "," << Yb << ") Est : " << result << "\n";
@@ -27,13 +51,13 @@ public:
 };
 
 int main() {
-	float result;
-	Distance d;
-	cout << "Coordonnees du premier point : ";
-	cin >> d.Xa >> d.Ya;
-	cout << "Coordonnees du deuxieme point : ";
-	cin >> d.Xb >> d.Yb;
+	string s;
+	float n1, n2, n3, n4;
+	cout << "Veuillez entrer les valeurs suivi du type de distance : \n";
+	cin >> n1 >> n2 >> n3 >> n4;
+	Distance* p = new Distance(n1, n2, n3, n4, s = "all");
+	p->check();
+	delete p;
 
-	d.all();
-
+	return 0;
 }
---
+++ ConsoleApplication1/ConsoleApplication1/corbeille p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Cla {
+public:
+
+	void afficher(void) {
+		cout << "Bonjour\n";
+	}
+};
+
+int main{
+	Cla l;
+	l* p;
+	p = new afficher;
+}
